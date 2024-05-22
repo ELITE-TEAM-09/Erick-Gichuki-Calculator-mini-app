370d394dbe45867b0ecd934b9478bb61739c5553
+ <+>
2024-05-22 11:39:21 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Erick-Gichuki---Calculator-mini-app
+## How to run the app
+- Run ```python eric\'s_calc.py``` on your terminal to start working.
+- If the user inputs non-numeric it prints error telling you to enter valid numbers.
+<img src='./image.png'/>
+- For the division if the user divides a number by zero it gives you an error "cannot divide by zero".
--- eric's_calc.py
+++ eric's_calc.py
@@ -8,10 +8,19 @@ def divide(a, b):
     if b == 0:
         return "Error: cannot divide by zero"
     return a // b
-a = int(input("Enter A: "))
-b = int(input("Enter B: "))
+def exponent(a, b):
+    return a ** b
+try:
+     a = int(input("Enter A: "))
+     b = int(input("Enter B: "))
 
-print(sum(a, b))
-print(minus(a, b))
-print(multiply(a, b))
-print(divide(a, b))
+     print(f"Sum: {sum(a, b)}")
+     print(f"Difference: {minus(a, b)}")
+     print(f"Product: {multiply(a, b)}")
+     print(f"Quotient: {divide(a, b)}")
+     print(f"Exponent: {exponent(a, b)}")
+except ValueError:
+    print("Error: please enter valid numbers")
+
+
+
